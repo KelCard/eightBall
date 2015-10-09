1aeaaa11c2d263ba22831514e14bf039e21a62ff
KelCard <kgerm29@yahoo.com>
2015-10-08 20:41:04 -0400

    finishing up the eightBall project.

--- eightBall.js
+++ eightBall.js
@@ -1,6 +1,6 @@
-var eightBall = { 
+var eightBall = {
 	index: 0,
-	advice: ["Yes", "No", "Maybe", "It's worth a shot", "No way", "I wouldn't try it", "Of course"],
+	advice: ["Yes", "No", "Maybe", "It's worth a shot", "No way", "I wouldn't try it", "Of course", "Go for it", "Try again"],
 	shake: function () {
 		this.index = Math.floor(Math.random() * this.advice.length);
 	},
@@ -11,9 +11,10 @@ var eightBall = {
 
 
 $("#button").click(function(){
+	var audio = new Audio('rollDice.mp3');
+	audio.play();
     $(".target").effect( "shake", {times:4}, 1000 );
 	eightBall.shake();
 	$("#result").show( "slow" );
 	$( "#result" ).html( "<b>Answer: </b> " + eightBall.look());
-	console.log(eightBall.look());
 });
